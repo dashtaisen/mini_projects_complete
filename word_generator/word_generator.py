058204560dea67050529b8e3112c9cd109c29bb4
Nicholas Miller <nicholas.anthony.miller@gmail.com>
2018-04-15 16:34:35 -0400

    Add morphology support

--- word_generator/word_generator.py
+++ word_generator/word_generator.py
@@ -144,22 +144,96 @@ def create_words(phon_csv,max_syls = 4):
         defs_and_words.add((definition,word))
     return defs_and_words
 
-def apply_morphology(word,pos):
+def create_morphemes(defs_and_words, phon_csv,morph_csv,max_syls=1):
+    """Create morphemes"""
+    morphemes = list() # list of morphemes
+    defs_and_morphemes = list() # list of (meaning,morpheme) tuples
+    initials, medials, finals = load_phon_template(phon_csv)
+    with open(morph_csv) as source:
+        # Get the morphs that exist in the language
+        reader = csv.DictReader(source)
+        morph_meanings = list()
+        for row in reader:
+            if int(row['in_lang']) == 1:
+                morph_meanings.append(row['morpheme'])
+    existing_words = [word for definition, word in defs_and_words]
+
+    # To ensure some variety, keep track of the prev word
+    prev_initial = ""
+    prev_medial = ""
+    prev_final = ""
+    cur_initial = ""
+    cur_medial = ""
+    cur_final = ""
+
+    for morph_meaning in morph_meanings:
+        morpheme = None
+        current_morpheme_parts = list()
+        while morpheme is None or morpheme in existing_words or morpheme in morphemes:
+            num_syls = random.randint(0,max_syls)
+            for syl in range(num_syls):
+                while cur_initial == prev_initial:
+                    cur_initial = random.choice(initials)
+                prev_initial = cur_initial
+                current_morpheme_parts.append(cur_initial)
+
+                while cur_medial == prev_medial:
+                    cur_medial = random.choice(medials)
+                prev_medial = cur_medial
+                current_morpheme_parts.append(cur_medial)
+
+                while cur_final == prev_final:
+                    cur_final = random.choice(finals)
+                prev_final = cur_final
+                current_morpheme_parts.append(cur_final)
+            morpheme = "".join(current_morpheme_parts)
+        morphemes.append(morpheme)
+        defs_and_morphemes.append((morph_meaning,morpheme))
+    return defs_and_morphemes
+
+def apply_morphology(word,pos,defs_and_morphemes):
     """Apply morphology to word"""
     word_conj = ""
     if pos == "noun":
-        noun_prefix = random.choice(NOUN_PREFIXES)
-        noun_suffix = random.choice(NOUN_SUFFIXES)
-        word_conj = "{}{}{}".format(noun_prefix,word,noun_suffix)
+        n_prefixes = [
+            morpheme for definition, morpheme in defs_and_morphemes
+            if definition.startswith("prep_")
+        ]
+        n_suffixes = [
+            morpheme for definition, morpheme in defs_and_morphemes
+            if definition.startswith("n_")
+        ]
+        noun_prefix = random.choice(n_prefixes)
+        noun_suffix = random.choice(n_suffixes)
+        word_conj = "{}-{}-{}".format(noun_prefix,word,noun_suffix)
+
     elif pos == "verb":
-        verb_prefix = random.choice(VERB_PREFIXES)
-        verb_suffix = random.choice(VERB_SUFFIXES)
-        word_conj = "{}{}{}".format(verb_prefix,word,verb_suffix)
+        verb_morphemes = [
+            (definition, morpheme) for definition, morpheme in defs_and_morphemes
+            if definition.startswith("v_")
+        ]
+        agr = list()
+        tense = list()
+        aspect = list()
+        other_v_suffix = list()
+        for definition, morpheme in verb_morphemes:
+            if "agr" in definition: agr.append(morpheme)
+            elif "aspect" in definition: aspect.append(morpheme)
+            elif "tense" in definition: tense.append(morpheme)
+            else: other_v_suffix.append(morpheme)
+        v_agr = random.choice(agr)
+        v_aspect = random.choice(aspect)
+        v_tense = random.choice(tense)
+        v_suffix = random.choice(other_v_suffix)
+        word_conj = "{}-{}-{}-{}".format(v_agr,v_aspect,word,v_tense,v_suffix)
 
     elif pos == "adjective":
-        adj_prefix = random.choice(ADJ_PREFIXES)
-        adj_suffix = random.choice(ADJ_SUFFIXES)
-        word_conj = "{}{}{}".format(adj_prefix,word,adj_suffix)
+        adj_morphemes = [
+            morpheme for definition, morpheme in defs_and_morphemes
+            if definition.startswith("a_")
+        ]
+        adj_suffix = random.choice(adj_morphemes)
+        word_conj = "{}-{}".format(word,adj_suffix)
     else:
         word_conj = word
     return word_conj
@@ -174,7 +248,7 @@ def random_little_word(little_word_proportion):
         little_word_choice = random.choice(LITTLE_WORDS)
     return little_word_choice
 
-def create_fake_text(defs_and_words,num_sents=10,max_sent_length=8,little_word_proportion=3):
+def create_fake_text(defs_and_words,defs_and_morphemes,num_sents=10,max_sent_length=8,little_word_proportion=3):
     """Create a fake text with no meaning"""
     text = list() # The created text
     words = [word for definition, word in defs_and_words]
@@ -182,7 +256,7 @@ def create_fake_text(defs_and_words,num_sents=10,max_sent_length=8,little_word_p
         current_sent = list()
         first_word = random.choice(words)
         first_pos = random.choice(POS)
-        first_word_conj = apply_morphology(first_word,first_pos)
+        first_word_conj = apply_morphology(first_word,first_pos,defs_and_morphemes)
         prev_word = first_word
         prev_pos = first_pos
         cur_word = first_word
@@ -198,7 +272,7 @@ def create_fake_text(defs_and_words,num_sents=10,max_sent_length=8,little_word_p
                 cur_pos = random.choice(POS)
             prev_word = cur_word
             prev_pos = cur_pos
-            cur_word_conj = apply_morphology(cur_word,cur_pos)
+            cur_word_conj = apply_morphology(cur_word,cur_pos,defs_and_morphemes)
             current_sent.append(cur_word_conj)
             little_word_choice = random_little_word(little_word_proportion)
             if little_word_choice != "":
@@ -210,13 +284,22 @@ def create_fake_text(defs_and_words,num_sents=10,max_sent_length=8,little_word_p
 if __name__ == "__main__":
     for lang_name, max_syl in [("lang01",3),("lang02",3),("lang03",1),("lang04",4)]:
         print("Creating language from {} template".format(lang_name))
-        csv_name = "{}_template.csv".format(lang_name)
-        filename = os.path.join(CONSTRAINTS_DIR,csv_name)
-        defs_and_words = create_words(filename,max_syls=max_syl)
+        phon_csv = "{}_phonology.csv".format(lang_name)
+        morph_csv = "{}_morphology.csv".format(lang_name)
+        phon_filename = os.path.join(CONSTRAINTS_DIR,phon_csv)
+        morph_filename = os.path.join(CONSTRAINTS_DIR,morph_csv)
+        defs_and_words = create_words(phon_filename,max_syls=max_syl)
         for definition, word in defs_and_words:
             print("{}: {}".format(definition, word))
         print()
+
+        print("Creating morphemes:")
+        defs_and_morphemes = create_morphemes(defs_and_words, phon_filename,morph_filename,max_syls=1)
+        for definition, morpheme in defs_and_morphemes:
+            print("{}: {}".format(definition, morpheme))
+        print()
+
         print("Creating sample text:")
-        fake_text = create_fake_text(defs_and_words)
+        fake_text = create_fake_text(defs_and_words,defs_and_morphemes)
         print(fake_text)
         print()
